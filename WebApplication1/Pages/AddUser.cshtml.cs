7de132e3c3e6dfa794591bcf26b78d344972691d
dolgi@.com <sokolovpi-23B>
2026-03-27 09:53:00 +0300

    27.03.2026

---
+++ WebApplication1/Pages/AddUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class AddUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
